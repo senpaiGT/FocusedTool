d0222affb96d761abcce5bc8bfe66c6633412710
senpaiGT <85949224+senpaiGT@users.noreply.github.com>
2021-06-15 23:56:18 +0300

    Add files via upload

---
+++ FocusedTool.sh
@@ -0,0 +1,30 @@
+clear
+echo -en "\e[93m Password: \e[m"
+read answer
+if [ "$answer" != "${answer#[roots]}" ] ;then
+tput cup 17 21
+clear
+
+else
+tput cup 17 23
+clear
+echo " <──[ Wrong Password ]──> "|lolcat
+sleep 1
+    bash FocusedTool.sh
+fi
+echo " "
+echo " "
+echo " "
+echo "
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭┳━━━━╮╱╱╱╱╭╮
+┃╭━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃╭╮╭╮┃╱╱╱╱┃┃
+┃╰━━┳━━┳━━┳╮╭┳━━┳━━┳━╯┣╯┃┃┣┻━┳━━┫┃
+┃╭━━┫╭╮┃╭━┫┃┃┃━━┫┃━┫╭╮┃╱┃┃┃╭╮┃╭╮┃┃
+┃┃╱╱┃╰╯┃╰━┫╰╯┣━━┃┃━┫╰╯┃╱┃┃┃╰╯┃╰╯┃╰╮
+╰╯╱╱╰━━┻━━┻━━┻━━┻━━┻━━╯╱╰╯╰━━┻━━┻━╯ BETA VERSİON "|lolcat
+echo " "
+echo " 
+[Ddoser Tool] --> git clone git clone https://github.com/cyweb/hammer
+[Termux Desinger] --> git clone git clone https://github.com/Bhaviktutorials/T-Remix "|lolcat
+echo " 
+<─────[ Hello Welcome To FocusedTool Is Beta Sorry ]─────> "|lolcat
---
+++ setup-Tool.sh
@@ -0,0 +1,45 @@
+echo -e "\e[32m[*]\e[34m WELCOME TO FocusedLG TOOLS  \e[m "
+sleep 5
+echo
+#Installing the following packages to install T-Remix
+echo " "
+echo -e "\e[32m[*]\e[34m Installing FocusedTool \e[m "
+apt-get update -y
+apt-get upgrade -y
+apt-get install python -y
+apt-get install figlet -y
+apt-get  install ruby -y
+pip install lolcat
+gem install lolcat
+apt-get install toilet -y
+pkg install ncurses-utils -y
+
+
+clear
+echo " "
+echo " "
+echo " "
+echo "
+
+        WELCOME TO  FocusedLG TOOLS
+    
+                
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭┳━━━━╮╱╱╱╱╭╮
+┃╭━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃╭╮╭╮┃╱╱╱╱┃┃
+┃╰━━┳━━┳━━┳╮╭┳━━┳━━┳━╯┣╯┃┃┣┻━┳━━┫┃
+┃╭━━┫╭╮┃╭━┫┃┃┃━━┫┃━┫╭╮┃╱┃┃┃╭╮┃╭╮┃┃
+┃┃╱╱┃╰╯┃╰━┫╰╯┣━━┃┃━┫╰╯┃╱┃┃┃╰╯┃╰╯┃╰╮
+╰╯╱╱╰━━┻━━┻━━┻━━┻━━┻━━╯╱╰╯╰━━┻━━┻━╯ v 2.0" |lolcat
+
+
+echo " "
+echo " "
+echo " 
+       <─────────────── [ Re-Coded by =*•.¸♡ senpaiGT ♡¸.•* ] ───────────────>            
+                                                               " |lolcat
+echo " "
+echo " "
+echo " 
+<─────[ Please Wait! ]─────> "|lolcat
+sleep (4)
+bash FocusedTool.sh
